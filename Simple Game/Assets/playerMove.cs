014190871f480844f5eefd7e85a5f1a24c6c993e
Moin Sharif <32494969+moinsharif@users.noreply.github.com>
2019-12-17 23:21:36 +0600

    Busket ball texture added

---
+++ Simple Game/Assets/playerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            rb.AddForce(new Vector3(5,0,0) * 5 * Time.deltaTime);
+        }
+    }
+}
