96705be1ff6f7adaa3ddd4267028b9080bc727a4
Moin Sharif <32494969+moinsharif@users.noreply.github.com>
2019-12-18 17:38:58 +0600

    Update playerMove.cs

--- Simple Game/Assets/playerMove.cs
+++ Simple Game/Assets/playerMove.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class playerMove : MonoBehaviour
 {
     public Rigidbody rb;
+    public Animator anim;
+    float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,9 +19,19 @@ public class playerMove : MonoBehaviour
     {
         if (Input.GetKeyDown("up"))
         {
+            //anim.SetTrigger("StartAnim");
             //rb.AddForce(new Vector3(5,0,0) * 5 * Time.deltaTime);
-            rb.AddForce(transform.forward * 5);
-
+            //rb.AddForce(transform.forward * 5);
+            //rb.velocity = new Vector3(0, 0, 1) * speed * Time.deltaTime;
+            Movement1();
         }
+    }
+
+    public void Movement1()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
     }
 }
